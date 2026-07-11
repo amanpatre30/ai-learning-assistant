d4cee301e0da7899dfbdefafb5f342ce074e2c6e
amanpatre <amanpatre321@gmail.com>
2026-07-11 17:01:00 +0530

    Fix progress dashboard authentication and update quiz controller

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ import authRoutes from "./routes/authRoutes.js";
 import documentRoutes from "./routes/documentRoutes.js";
 import flashcardRoutes from "./routes/flashcardRoutes.js";
 import aiRoutes from "./routes/aiRoutes.js";
+import Quiz from "./models/Quiz.js";
+import quizRoutes from "./routes/quizRoutes.js";
+
+import progressRoutes from "./routes/progressRoute.js";
 //ES6 module __dirname alternavtive
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -50,6 +54,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/documents", documentRoutes);
 app.use("/api/flashcards", flashcardRoutes);
 app.use("/api/ai", aiRoutes);
+app.use("/api/quizzes", quizRoutes);
+app.use("/api/progress", progressRoutes);
 
 app.use(errorHandler);
 //404 handler
